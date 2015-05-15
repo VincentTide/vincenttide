c72bec539d99ef587ac41fcc2924cfd91753b27a
Vincent Zhang <vincenttide@gmail.com>
2015-05-15 16:36:55 -0400

    added favicon

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = 'insert-secret-key'
 SITE_TITLE = 'vincenttide'
 DEFAULT_ROLE = 'user'
-ARTICLES_PER_PAGE_HOMEPAGE = 10
+ARTICLES_PER_PAGE_HOMEPAGE = 7
 ARTICLES_PER_PAGE_ADMIN = 20
 
 from config_dev import *
